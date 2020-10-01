12143eeccb90c29ef7574246a06fdbae802ec4d3
Denis <alehin.de@gmail.com>
2020-10-01 13:52:21 +0300

    Made an element deleting

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Enter a array size(>= 10):");
 
         // Entering array size
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a array size(>= 10):");
         int arrSize = 0;
         do {
             try {
@@ -20,6 +20,21 @@ public class Solution {
             }
         } while(arrSize < 10);
 
+        // Declaration of a variable
+        int[] array = new int[arrSize];
+        for (int i = 0; i < arrSize; i++)
+            array[i] = i + 1;
+
+        // Choosing an element to remove
+        Random rand = new Random();
+        int elementToBeRemoved = rand.nextInt(arrSize);
+
+        // Deleting the element
+        int [] newArray = new int[arrSize - 1];
+        for (int i = 0; i < arrSize - 1; i++)
+            newArray[i] = array[i < elementToBeRemoved? i: i+1];
+
+        
 
     }
 }
