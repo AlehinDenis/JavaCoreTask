e46bbc01169899d4f96f331ac00f3e0326ec9c00
Denis <alehin.de@gmail.com>
2020-10-01 14:09:47 +0300

    Shuffle done

--- src/Solution.java
+++ src/Solution.java
@@ -20,9 +20,9 @@ public class Solution {
             }
         } while(arrSize < 10);
 
-        // Declaration of a variable
+        // Declaration of a variable ????? CHANGE
         int[] array = new int[arrSize];
-        for (int i = 0; i < arrSize; i++)
+        for (int i = 0; i < array.length; i++)
             array[i] = i + 1;
 
         // Choosing an element to remove
@@ -31,10 +31,23 @@ public class Solution {
 
         // Deleting the element
         int [] newArray = new int[arrSize - 1];
-        for (int i = 0; i < arrSize - 1; i++)
+        for (int i = 0; i < newArray.length; i++)
             newArray[i] = array[i < elementToBeRemoved? i: i+1];
 
+        shuffleArray(newArray);
         
-
     }
+
+    public static  void shuffleArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++) {
+            Random rand = new Random();
+            int j = rand.nextInt(array.length);
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
 }
